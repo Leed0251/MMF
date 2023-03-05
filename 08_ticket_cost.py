40d61251c15019c32b9deea79f3ea9f1c8d03d96
Leed0251 <99302462+Leed0251@users.noreply.github.com>
2023-03-06 10:39:12 +1300

    Update 08_ticket_cost.py
    Fixed the length of all_ticket_costs list allowing code to function

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -2,7 +2,7 @@ import pandas
 
 # dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
-all_ticket_costs = [7.50, 7.50, 10.50, 6.50]
+all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
 surcharge = [0, 0, 0.53, 0.53, 0]
 
 mini_movie_dict = {
@@ -14,8 +14,7 @@ mini_movie_dict = {
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
 
 # Calculate the total ticket cost (ticket + surcharge)
-mini_movie_frame["Total"] = mini_movie_frame["Surcharge"] \
-    + mini_movie_frame["Ticket Price"]
+mini_movie_frame["Total"] = mini_movie_frame["Surcharge"] + mini_movie_frame["Ticket Price"]
 
 # Calculate the profit for each ticket
 mini_movie_frame["Profit"] = mini_movie_frame["Ticket Price"] - 5
